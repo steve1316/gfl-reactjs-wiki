26495981ffe11de5f99281e1796384c23c4134f8
Steve Tu <stevetu11@yahoo.com>
2020-08-19 23:27:40 -0700

    Made lots of changes again so read description
    * Added Tooltip and MOD filter to T-Doll Index.
    * Able to switch between Normal and MOD in T-Doll page.
    * Skill information is shown now. Currently bugged due to new state changes.

--- src/data/tdolls_from_1_to_10.js
+++ src/data/tdolls_from_1_to_10.js
@@ -3,51 +3,73 @@
     - All stats are considered at max rank. For skill stats, I'm separating them for now.
     - If T-Doll has MOD, mod attribute will be populated. If not, mod attribute will be set to NULL.
 	- number_of_stats in skill attribute dictates the amount of numbers from stat(1) to stat(n) to be replacing the "#(n)"" delimiters inside the skill description in sequential order.
+	- TODO: include full body pictures as well.
 	- TODO: include their sprites as well.
+	- TODO: include pictures of skills as well.
+	- TODO: include Tile information as well.
+	- TODO: differentiate between Passive and/or Active skills.
 
 */
 
 const tdolls = [
 	{
-		id: 1,
-		name: "SAA",
-		type: "hg",
-		rarity: 4,
-		max_hp: 80,
-		max_dmg: 36,
-		max_acc: 49,
-		max_eva: 79,
-		max_rof: 47,
-		skill: {
-			name: "Fire Command",
-			initial_cooldown: 6,
-			cooldown: [15, 14.7, 14.3, 14, 13.7, 13.3, 13, 12.7, 12.4, 12],
-			description: "Increase all allies' damage by #1 for #2 seconds.",
-			number_of_stats: 2,
-			stat1: [15, 16, 17, 18, 19, 21, 22, 23, 24, 25],
-			stat2: [5, 6, 6, 6, 7, 7, 7, 8, 8, 8]
+		normal: {
+			id: 1,
+			name: "SAA",
+			type: "HG",
+			rarity: 4,
+			max_hp: 80,
+			max_dmg: 36,
+			max_acc: 49,
+			max_eva: 79,
+			max_rof: 47,
+			skill: {
+				name: "Fire Command",
+				initial_cooldown: 6,
+				cooldown: [15, 14.7, 14.3, 14, 13.7, 13.3, 13, 12.7, 12.4, 12],
+				description: "Increase all allies' damage by #1 for #2 seconds.",
+				number_of_stats: 2,
+				stat1: ["15%", "16%", "17%", "18%", "19%", "21%", "22%", "23%", "24%", "25%"],
+				stat2: [5, 6, 6, 6, 7, 7, 7, 8, 8, 8],
+				image_skill: require("../images/tdolls/fire_command.jpg")
+			},
+			image_normal: require("../images/tdolls/1_normal.png"),
+			image_damaged: require("../images/tdolls/1_damaged.png")
 		},
-		image_normal: require("../images/tdolls/1_normal.jpg"),
-		image_damaged: require("../images/tdolls/1_damaged.jpg"),
 		mod: {
+			id: 1,
+			name: "SAA Mod",
+			type: "HG",
 			rarity: 5,
 			max_hp: 83,
 			max_dmg: 37,
 			max_acc: 51,
 			max_eva: 80,
 			max_rof: 50,
+			skill: {
+				name: "Fire Command",
+				initial_cooldown: 6,
+				cooldown: [15, 14.7, 14.3, 14, 13.7, 13.3, 13, 12.7, 12.4, 12],
+				description: "Increase all allies' damage by #1 for #2 seconds.",
+				number_of_stats: 2,
+				stat1: ["15%", "16%", "17%", "18%", "19%", "21%", "22%", "23%", "24%", "25%"],
+				stat2: [5, 6, 6, 6, 7, 7, 7, 8, 8, 8],
+				image_skill: require("../images/tdolls/fire_command.jpg")
+			},
 			skill2: {
 				name: "Duel Survivor",
 				initial_cooldown: 6,
 				cooldown: [15, 14.7, 14.3, 14, 13.7, 13.3, 13, 12.7, 12.4, 12],
-				description: "Increase all allies' rate of fire and accuracy by #1 for every #2 seconds that SAA is alive in battle. Max 3 stacks.",
+				description: "Increase all allies' rate of fire and accuracy by #1% for every #2 seconds that SAA is alive in battle. Max 3 stacks.",
 				number_of_stats: 2,
 				stat1: [3, 3, 3, 4, 4, 4, 5, 5, 5, 5],
-				stat2: [6, 5.8, 5.6, 5.3, 5.1, 4.9, 4.7, 4.4, 4.2, 4]
+				stat2: [6, 5.8, 5.6, 5.3, 5.1, 4.9, 4.7, 4.4, 4.2, 4],
+				image_skill: require("../images/tdolls/duel_survivor.jpg")
 			},
-			image_normal: require("../images/tdolls/1_mod_normal.jpg"),
-			image_damaged: require("../images/tdolls/1_mod_damaged.jpg")
-		}
+			image_normal: require("../images/tdolls/1_mod_normal.png"),
+			image_damaged: require("../images/tdolls/1_mod_damaged.png")
+		},
+		selected: {}
 	}
 ];
 
--- src/pages/tdoll/TDoll.jsx
+++ src/pages/tdoll/TDoll.jsx
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+
+import parse from "html-react-parser"; // This is needed to parse the span tags inserted into the skill description strings.
+import PropTypes from "prop-types";
 
 // MaterialUI imports
-import { Container, makeStyles, Grid, Typography, Card, CardMedia, CardActionArea, CardContent, TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
+import {
+	Container,
+	makeStyles,
+	Grid,
+	Typography,
+	Card,
+	CardMedia,
+	CardActionArea,
+	CardContent,
+	TableContainer,
+	Paper,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+	CardHeader,
+	Avatar,
+	Select,
+	MenuItem,
+	InputLabel,
+	FormControl,
+	Tabs,
+	Tab,
+	withStyles
+} from "@material-ui/core";
 
 // MaterialUI icon imports
 import StarIcon from "@material-ui/icons/Star";
+import { useEffect } from "react";
 
 // Image imports
 //import rarity_star from "../../images/rarity_star.png";
+
+const StyledTabs = withStyles({
+	indicator: {
+		display: "flex",
+		justifyContent: "center",
+		backgroundColor: "transparent",
+		"& > span": {
+			maxWidth: 40,
+			width: "100%",
+			backgroundColor: "#635ee7"
+		}
+	}
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+	root: {
+		textTransform: "none",
+		color: "#fff",
+		fontWeight: theme.typography.fontWeightRegular,
+		fontSize: theme.typography.pxToRem(15),
+		marginRight: theme.spacing(1),
+		"&:focus": {
+			opacity: 1
+		}
+	}
+}))((props) => <Tab {...props} />);
 
 export default function TDoll(props) {
 	const useStyles = makeStyles((theme) => ({
@@ -20,23 +75,25 @@ export default function TDoll(props) {
 		},
 		card: {
 			height: "100%",
-			display: "flex",
-			flexDirection: "column"
+			width: "100%",
+			display: "flex"
 		},
 		cardForImage: {
 			display: "flex",
-			flexDirection: "column",
-			maxWidth: 200,
-			maxHeight: 550,
+			width: 256, // The dimensions of the images.
+			height: 512,
 			marginBottom: 10
+		},
+		cardForSkill: {
+			display: "flex",
+			minWidth: 300
 		},
 		cardMedia: {
 			paddingTop: "56.25%" // 16:9
 		},
 		cardMediaForImage: {
-			height: "100%",
-			width: "100%",
-			objectFit: "contain"
+			// height: "100%",
+			// objectFit: "contain"
 		},
 		cardContent: {
 			flexGrow: 1
@@ -54,13 +111,26 @@ export default function TDoll(props) {
 		},
 		rarityStar: {
 			display: "inline-block",
-			transform: "translate(0px,3px)", // This will set the rendered stars to be about inline with the T-Doll's type text.
+			transform: "translate(0px, 3px)", // This will set the rendered stars to be about inline with the T-Doll's type text.
 			marginLeft: 3
 		},
 		table: {
-			minWidth: 100,
+			minWidth: 150,
 			maxWidth: 200,
 			backgroundColor: theme.palette.grey[600]
+		},
+		title: {
+			fontSize: 14
+		},
+		pos: {
+			marginBottom: 12
+		},
+		skillLevel: {
+			//
+		},
+		tabs: {
+			width: 256,
+			backgroundColor: theme.palette.grey[900]
 		}
 	}));
 
@@ -87,13 +157,87 @@ export default function TDoll(props) {
 		return stars;
 	};
 
-	// Grab the T-Doll's information from the sessionStorage
+	// Grab the T-Doll's information from the sessionStorage.
 	const id = props.location.search.substring(4);
-	const tdoll = JSON.parse(sessionStorage.getItem(id));
+	const [tdoll, setTDoll] = useState(JSON.parse(sessionStorage.getItem(id)));
 
-	console.log(tdoll);
+	// Set initial states.
+	const [skillLevel, setSkillLevel] = useState(10);
+	const [tdollImage, setTDollImage] = useState(undefined);
+	const [switchImage, setSwitchImage] = useState(false);
+	const [MOD, setMOD] = useState(false);
+	const [value, setValue] = useState(0); // 0 for Normal, 1 for MOD.
 
-	const handleChangeImage = () => {};
+	useEffect(() => {
+		tdoll.selected = tdoll.normal;
+
+		// Console log the T-Doll's information only once.
+		console.log("T-Doll: ", tdoll);
+
+		setTDollImage(tdoll.selected.image_normal);
+	}, []);
+
+	const handleChangeImage = () => {
+		// Replace the T-Doll's image with normal or damaged versions.
+		if (switchImage) {
+			setTDollImage(tdoll.selected.image_normal);
+			//console.log("Normal art is showed.");
+			setSwitchImage(false);
+		} else {
+			setTDollImage(tdoll.selected.image_damaged);
+			//console.log("Damaged art is showed.");
+			setSwitchImage(true);
+		}
+	};
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	useEffect(() => {
+		// Switch the information shown from Normal to MOD for the T-Doll depending on the tab selected above its image.
+		tdoll.selected = {};
+		if (value === 1) {
+			//console.log("Setting to MOD");
+			tdoll.selected = tdoll.mod;
+			setTDollImage(tdoll.selected.image_normal);
+		} else {
+			//console.log("Setting to Normal");
+			tdoll.selected = tdoll.normal;
+			setTDollImage(tdoll.selected.image_normal);
+		}
+	}, [value]);
+
+	useEffect(() => {
+		// A hack-job attempt at programmatically replacing all delimiters with the appropriate stat numbers at the chosen skill level.
+		// It will also insert into the strings some <span> and <ins> tags in order to highlight what stats are changed when the skill level changes.
+		// The npm package html-react-parser will parse the inserted span tags and properly render them into HTML tags.
+		// Note: The styling being inserted is using HTML styling and not using React styling.
+		var tdoll_temp = tdoll; // Make a temp copy of this JSON state.
+		var skillDescription = tdoll_temp.selected.skill.description;
+		var numberOfStats = tdoll_temp.selected.skill.number_of_stats;
+		switch (numberOfStats) {
+			case 1:
+				skillDescription = skillDescription.replace("#1", '<span style="color: cyan;"><ins>' + tdoll_temp.selected.skill.stat1[skillLevel - 1] + "</ins></span>");
+				break;
+			case 2:
+				skillDescription = skillDescription.replace("#1", '<span style="color: cyan; font-size: 125%;"><ins>' + tdoll_temp.selected.skill.stat1[skillLevel - 1] + "</ins></span>");
+				skillDescription = skillDescription.replace("#2", '<span style="color: cyan; font-size: 125%;"><ins>' + tdoll_temp.selected.skill.stat2[skillLevel - 1] + "</ins></span>");
+				break;
+			case 3:
+				skillDescription = skillDescription.replace("#1", '<span style="color: cyan;"><ins>' + tdoll_temp.selected.skill.stat1[skillLevel - 1] + "</ins></span>");
+				skillDescription = skillDescription.replace("#2", '<span style="color: cyan;"><ins>' + tdoll_temp.selected.skill.stat2[skillLevel - 1] + "</ins></span>");
+				skillDescription = skillDescription.replace("#3", '<span style="color: cyan;"><ins>' + tdoll_temp.selected.skill.stat3[skillLevel - 1] + "</ins></span>");
+				break;
+
+			default:
+		}
+
+		console.log("Saving skill description");
+
+		// Save the properly formatted skill description.
+		setTDoll(tdoll_temp);
+	}, [skillLevel]);
 
 	return (
 		<main>
@@ -101,29 +245,36 @@ export default function TDoll(props) {
 				<br />
 
 				<Card className={classes.card}>
-					<CardContent>
+					<CardContent className={classes.cardContent}>
+						{/* T-Doll's Name, Rarity in stars, type, and Index Number */}
 						<Typography className={classes.rarityStars} color="textSecondary" gutterBottom>
-							{tdoll.type.toUpperCase()}
-							{renderStars(tdoll.rarity)}
+							{tdoll.selected.type}
+							{renderStars(tdoll.selected.rarity)}
 						</Typography>
 						<Typography variant="h3" component="h2">
-							{tdoll.name}
+							{tdoll.selected.name}
 							<Typography display="inline" color="textSecondary">
 								{" "}
-								#{tdoll.id}
+								#{tdoll.selected.id}
 							</Typography>
 						</Typography>
 
-						<Grid container direction="row" justify="center" alignItems="center">
-							<Grid item key="T-Doll image" xs={12} sm={6}>
+						{/* T-Doll image */}
+						<Grid container direction="row" spacing={2}>
+							<Grid item key="T-Doll image" xs={6}>
+								<Tabs className={classes.tabs} value={value} onChange={handleChange} indicatorColor="primary" textColor="primary">
+									<Tab label="Normal" />
+									<Tab label="MOD" />
+								</Tabs>
 								<Card className={classes.cardForImage} elevation={12}>
 									<CardActionArea onClick={handleChangeImage}>
-										<CardMedia component="img" className={classes.cardMediaForImage} image={tdoll.image_normal} title={tdoll.name} />
+										<CardMedia component="img" className={classes.cardMediaForImage} image={tdollImage} title={tdoll.selected.name} />
 									</CardActionArea>
 								</Card>
 							</Grid>
 
-							<Grid item key="T-Doll stat table" xs={12} sm={6}>
+							{/* T-Doll's stats in table format */}
+							<Grid item key="T-Doll stat table" xs={10} sm={6}>
 								<TableContainer className={classes.table} component={Paper} elevation={12}>
 									<Table>
 										<TableHead>
@@ -137,35 +288,97 @@ export default function TDoll(props) {
 												<TableCell component="th" scope="row">
 													HP
 												</TableCell>
-												<TableCell align="right">{tdoll.max_hp}</TableCell>
+												<TableCell align="right">{tdoll.selected.max_hp}</TableCell>
 											</TableRow>
 											<TableRow>
 												<TableCell component="th" scope="row">
 													DMG
 												</TableCell>
-												<TableCell align="right">{tdoll.max_dmg}</TableCell>
+												<TableCell align="right">{tdoll.selected.max_dmg}</TableCell>
 											</TableRow>
 											<TableRow>
 												<TableCell component="th" scope="row">
 													ACC
 												</TableCell>
-												<TableCell align="right">{tdoll.max_acc}</TableCell>
+												<TableCell align="right">{tdoll.selected.max_acc}</TableCell>
 											</TableRow>
 											<TableRow>
 												<TableCell component="th" scope="row">
 													EVA
 												</TableCell>
-												<TableCell align="right">{tdoll.max_eva}</TableCell>
+												<TableCell align="right">{tdoll.selected.max_eva}</TableCell>
 											</TableRow>
 											<TableRow>
 												<TableCell component="th" scope="row">
 													ROF
 												</TableCell>
-												<TableCell align="right">{tdoll.max_rof}</TableCell>
+												<TableCell align="right">{tdoll.selected.max_rof}</TableCell>
 											</TableRow>
 										</TableBody>
 									</Table>
 								</TableContainer>
+							</Grid>
+
+							{/* T-Doll's skill information */}
+							<Grid item key="T-Doll skill(s)" xs={6} sm={6}>
+								<Card className={classes.cardForSkill} elevation={12}>
+									<CardContent>
+										<CardHeader
+											avatar={<Avatar variant="rounded" src={tdoll.selected.skill.image_skill} />}
+											title={tdoll.selected.skill.name}
+											subheader={"Initial CD: " + tdoll.selected.skill.initial_cooldown + "s"}
+											action={
+												<FormControl>
+													<InputLabel id="skill-level-select-label">Level</InputLabel>
+
+													<Select
+														id="skill-level-select"
+														className={classes.skillLevel}
+														value={skillLevel}
+														onChange={(e) => {
+															setSkillLevel(e.target.value);
+														}}
+														// MenuProps will shift the drop down menu to the right
+														MenuProps={{
+															anchorOrigin: {
+																vertical: "top",
+																horizontal: "right"
+															},
+															transformOrigin: {
+																vertical: "top",
+																horizontal: "left"
+															},
+															getContentAnchorEl: null
+														}}
+													>
+														<MenuItem value={1}>1</MenuItem>
+														<MenuItem value={2}>2</MenuItem>
+														<MenuItem value={3}>3</MenuItem>
+														<MenuItem value={4}>4</MenuItem>
+														<MenuItem value={5}>5</MenuItem>
+														<MenuItem value={6}>6</MenuItem>
+														<MenuItem value={7}>7</MenuItem>
+														<MenuItem value={8}>8</MenuItem>
+														<MenuItem value={9}>9</MenuItem>
+														<MenuItem value={10}>10</MenuItem>
+													</Select>
+												</FormControl>
+											}
+										/>
+										<Typography className={classes.title} color="textSecondary" gutterBottom>
+											{/* This will render the span tags inserted into the skill description and will color the numbers. */}
+											{parse(tdoll.selected.skill.description)}
+										</Typography>
+										<Typography className={classes.pos} color="textSecondary">
+											Cooldown:{" "}
+											{
+												<span style={{ color: "cyan" }}>
+													<ins>{tdoll.selected.skill.cooldown[skillLevel - 1]}s</ins>
+												</span>
+											}
+										</Typography>
+									</CardContent>
+								</Card>
 							</Grid>
 						</Grid>
 					</CardContent>
