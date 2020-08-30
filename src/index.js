807e4d848682ddf57c3e69c222754d99798ae684
Steve Tu <stevetu11@yahoo.com>
2020-08-30 13:33:04 -0700

    Added functionality to pages to scroll to top

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import ScrollToTop from "react-router-scroll-top";
+
 // HashRouter is needed for GitHub Pages to properly route users.
 import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
 	<Router>
-		<App />
+		<ScrollToTop>
+			<App />
+		</ScrollToTop>
 	</Router>,
 	document.getElementById("root")
 );
