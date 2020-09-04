565d95ae3e06e1c945d98d1804f2590c7abf32fb
Steve Tu <stevetu11@yahoo.com>
2020-09-03 17:22:04 -0700

    Added the last remaining pages

---
+++ src/pages/hoc_index/HOC_Index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { makeStyles, Container, Typography } from "@material-ui/core";
+
+// Component imports
+import ScrollToTop from "../../components/ScrollToTop";
+
+export default function HOC_Index() {
+	const useStyles = makeStyles((theme) => ({
+		root: {
+			marginTop: "5rem"
+		}
+	}));
+
+	const classes = useStyles();
+
+	return (
+		<main className={classes.root}>
+			<ScrollToTop />
+			<Container>
+				<Typography component="h1" variant="h5" align="center" color="textPrimary" gutterBottom>
+					Page under construction!
+				</Typography>
+			</Container>
+		</main>
+	);
+}
