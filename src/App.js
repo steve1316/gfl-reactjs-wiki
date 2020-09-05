b60d2b4f5943cb38a3587a75ad7c1d27668beae0
Steve Tu <stevetu11@yahoo.com>
2020-09-04 19:06:32 -0700

    Testing out Route transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { spring, AnimatedSwitch } from "react-router-transition";
 
 // MaterialUI imports
 import { orange, deepOrange } from "@material-ui/core/colors";
@@ -38,13 +39,49 @@ export default function App() {
 		}
 	});
 
+	// we need to map the `scale` prop we define below
+	// to the transform style property
+	function mapStyles(styles) {
+		return {
+			opacity: styles.opacity,
+			transform: `scale(${styles.scale})`
+		};
+	}
+
+	// wrap the `spring` helper to use a bouncy config
+	function bounce(val) {
+		return spring(val, {
+			stiffness: 100,
+			damping: 15
+		});
+	}
+
+	// child matches will...
+	const bounceTransition = {
+		// start in a transparent, upscaled state
+		atEnter: {
+			opacity: 0,
+			scale: 0.5
+		},
+		// leave in a transparent, downscaled state
+		atLeave: {
+			opacity: bounce(0),
+			scale: bounce(2)
+		},
+		// and rest at an opaque, normally-scaled state
+		atActive: {
+			opacity: bounce(1),
+			scale: bounce(1)
+		}
+	};
+
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<Navbar />
 			<div className="App">
 				{/* Switch is needed to make sure that the error from ErrorBoundary does not propagate up the component tree. */}
-				<Switch>
+				<AnimatedSwitch atEnter={bounceTransition.atEnter} atLeave={bounceTransition.atLeave} atActive={bounceTransition.atActive} mapStyles={mapStyles} className="route-wrapper">
 					<Route exact path="/" component={Home} />
 					<Route path="/reload" component={null} />
 					<Route path="/index" component={TDoll_Index} />
@@ -55,7 +92,7 @@ export default function App() {
 					<ErrorBoundary>
 						<Route path="/tdoll" component={TDoll} />
 					</ErrorBoundary>
-				</Switch>
+				</AnimatedSwitch>
 			</div>
 		</ThemeProvider>
 	);
